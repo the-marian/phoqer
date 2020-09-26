c3c28d77d8164e8201cd938c6e290461b273b615
Marian <maryan.zozulia@gmail.com>
2020-09-26 17:36:55 +0200

    test CI/CD after fix

--- backend/share/urls.py
+++ backend/share/urls.py
@@ -21,7 +21,7 @@ from django.urls import path
 from upload.views import image_upload
 
 urlpatterns = [
-    path("upload/", image_upload, name="upload"),
+    path("upload-media/", image_upload, name="upload"),
     path("admin/", admin.site.urls),
 ]
 
