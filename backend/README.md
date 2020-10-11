a5f3b2f4e1a348135f45a2cf442cf7c8870b4eb9
Marian <maryan.zozulia@gmail.com>
2020-10-11 15:17:35 +0200

    - add API api/v1/offers/search documentation in /backend/README.md - add interactive documentation url /swagger/ and /redoc/ powered by drf_yasg - replace pytest.ini with united config file setup.cfg for pytest mypy and other - add coverage library to count code test coverage

---
+++ backend/README.md
@@ -0,0 +1,41 @@
+***Search***
+
+**endpoint**
+
+api/v1/offers/search/
+
+**query Params**
+
+- category=spor (any category slug)
+- sub_category=bicycles (any sub_category slug)
+- city=Киев (пока-что это просто text_field)
+
+    *Ordering*
+
+- ordering=pud_date (ascending)
+- ordering=-pud_date (descending) Также работает сортировка по: (views, price, deposit_val). Можно сортировать одновременно по нескольким полям. Пример (ordering=views,price)
+
+seach=Marine ( ищет по полям title и description ) на английском языке не чуствительно к регистру, к сожалению на кирилице чуствительно, это баг который я хз пока как решить
+
+Пример одного такого запроса:
+```
+http://127.0.0.1:8000/api/v1/offers/search/?category=sport&city=%D0%9A%D0%B8%D0%B5%D0%B2&ordering=-pud_date&seach=Marine&&sub_category=bicycles
+```
+Вернет вот что:
+
+```
+[
+    {
+        "cover_image": "https://petavasport.kz/wp-content/uploads/2018/05/WhatsApp-Image-2018-05-27-at-17.51.18.jpeg",
+        "currency": "UAH",
+        "description": "Сдаю в аренду велосипед, стоит без дела жалко добру пропадать",
+        "id": "21446b4c-68b9-4ff4-abfe-1a4c921bd34b",
+        "is_favorite": false,
+        "per": "DAY",
+        "price": 100,
+        "pud_date": "2020-10-11",
+        "title": "Велосипед Marine Four Corners 2020",
+        "views": 0
+    }
+]
+```
--- backend/share/urls.py
+++ backend/share/urls.py
@@ -1,24 +1,25 @@
-"""share URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from upload.views import image_upload
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("upload/", image_upload, name="upload"),
@@ -27,6 +28,11 @@ urlpatterns = [
     path('api/v1/categories/', include('categories.urls', namespace='categories')),
     path('api/v1/offers/', include('offers.urls', namespace='offers')),
     # path('api/v1/users/', include('users.urls', namespace='users'))
+    # SWAGGER URL
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
 if bool(settings.DEBUG):
