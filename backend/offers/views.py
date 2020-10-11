2f8c2e176c2e3e8aafa27a623ebcb0fc65bd71a8
Marian <maryan.zozulia@gmail.com>
2020-10-11 02:50:46 +0200

    - add offers/search/ API endpoint, docs will be added soon to - add MyPy support and setup - deposit_val field now had default=0 - local dev server now work without any env vars

--- backend/offers/views.py
+++ backend/offers/views.py
@@ -1,4 +1,8 @@
+from django.db.models import QuerySet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import ListAPIView
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from .models import Offer
@@ -7,26 +11,57 @@ from .serializers import OfferListItemSerializer
 
 class PopularOffersView(ListAPIView):
     serializer_class = OfferListItemSerializer
+    queryset = Offer.objects.filter(status='ACTIVE').order_by('-promote_til_date', 'views')
 
-    def get_queryset(self):
-        queryset = Offer.objects.order_by('-promote_til_date', 'views')
-        username = self.request.query_params.get('username', None)
-        if username is not None:
-            queryset = queryset.filter(purchaser__username=username)
-        return queryset
-
-    def list(self, request, *args, **kwargs):
+    def list(self, request: Request, *args, **kwargs) -> Response:
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
 
 class SearchOffersView(ListAPIView):
-    queryset = Offer.objects.order_by('views')
     serializer_class = OfferListItemSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = [
+        'category',
+        'status',
+        'city',
+        'sub_category',
+    ]
+    search_fields = [
+        'description',
+        'title',
+    ]
+    ordering_fields = [
+        'pud_date',
+        'views',
+        'price',
+        'deposit_val',
+    ]
 
-    def list(self, request, *args, **kwargs):
-        # Note the use of `get_queryset()` instead of `self.queryset`
-        queryset = self.get_queryset()
-        serializer = OfferListItemSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self) -> 'QuerySet[Offer]':
+        query_params = self.request.query_params
+        max_price = query_params.get('max_price')
+        min_price = query_params.get('min_price')
+        no_deposit = query_params.get('no_deposit')
+        max_deposit = query_params.get('max_deposit')
+        min_deposit = query_params.get('min_deposit')
+
+        # because of "shadow name 'filters' from outer scope" warning, I name it '_filters'
+        _filters = {'status': 'ACTIVE'}
+        if max_price:
+            _filters['price__lte'] = max_price
+        if min_price:
+            _filters['price__gte'] = min_price
+        if no_deposit:
+            _filters['deposit_val__exact'] = '0'
+        if max_deposit:
+            _filters['deposit_val__lte'] = max_deposit
+        if min_deposit:
+            _filters['deposit_val__gte'] = min_deposit
+
+        return Offer.objects.filter(**_filters)
