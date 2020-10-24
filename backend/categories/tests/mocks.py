98fedb000a823e095bee0e540b4e99e2c51c80b4
Marian <maryan.zozulia@gmail.com>
2020-10-24 02:09:50 +0200

    setup tests structure

--- backend/categories/tests/mocks.py
+++ backend/categories/tests/mocks.py
@@ -1,8 +1,3 @@
-import pytest
-from rest_framework import status
-
-from .models import ParentCategories, ChildCategories
-
 sport_img_url = 'https://example.com/sport.jpeg'
 books_img_url = 'https://example.com/books.jpeg'
 toys_img_url = 'https://example.com/toys.jpeg'
@@ -136,59 +131,3 @@ list_categories_ordered_by_priority = [
         ]
     },
 ]
-
-
-@pytest.fixture
-def api_client():
-    from rest_framework.test import APIClient
-    return APIClient()
-
-
-@pytest.fixture
-def db_test_data(db):
-    sport = ParentCategories.objects.create(name='Sport', slug='sport', image=sport_img_url, is_active=True,
-                                            priority=1)
-    books = ParentCategories.objects.create(name='Books', slug='books', image=books_img_url, is_active=True,
-                                            priority=2)
-    toys = ParentCategories.objects.create(name='Toys', slug='toys', image=toys_img_url, is_active=True,
-                                           priority=3)
-    ChildCategories.objects.create(name='Bicycle', slug='bicycle', parent=sport)
-    ChildCategories.objects.create(name='Jet Ski', slug='jet-ski', parent=sport)
-    ChildCategories.objects.create(name='Bible', slug='bible', parent=books)
-    ChildCategories.objects.create(name='Alphabet', slug='alphabet', parent=books)
-    ChildCategories.objects.create(name='Yo-yo', slug='yo-yo', parent=toys)
-    ChildCategories.objects.create(name='Chess', slug='chess', parent=toys)
-
-
-def test_list_categories(api_client, db_test_data):
-    response = api_client.get(categories_endpoint_url)
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == list_categories_expected_response
-
-
-def test_is_active(api_client, db_test_data):
-    response = api_client.get(categories_endpoint_url)
-    assert response.json() == list_categories_expected_response
-
-    toys = ParentCategories.objects.get(name='Toys')
-    toys.is_active = False
-    toys.save()
-
-    response = api_client.get(categories_endpoint_url)
-    assert response.json() == list_categories_without_toys
-
-
-def test_priority(api_client, db_test_data):
-    response = api_client.get(categories_endpoint_url)
-    assert response.json() == list_categories_expected_response
-
-    sport = ParentCategories.objects.get(name='Sport')
-    sport.priority = 3
-    sport.save()
-
-    toys = ParentCategories.objects.get(name='Toys')
-    toys.priority = 1
-    toys.save()
-
-    response = api_client.get(categories_endpoint_url)
-    assert response.json() == list_categories_ordered_by_priority
---
+++ backend/categories/tests/tests.py
@@ -0,0 +1,43 @@
+from rest_framework import status
+
+from categories.models import ParentCategories
+from categories.tests.mocks import (
+    categories_endpoint_url,
+    list_categories_expected_response,
+    list_categories_ordered_by_priority,
+    list_categories_without_toys,
+)
+
+
+def test_list_categories(api_client, db_test_data):
+    response = api_client.get(categories_endpoint_url)
+    assert response.status_code, status.HTTP_200_OK
+    assert response.json() == list_categories_expected_response
+
+
+def test_is_active(api_client, db_test_data):
+    response = api_client.get(categories_endpoint_url)
+    assert response.json() == list_categories_expected_response
+
+    toys = ParentCategories.objects.get(name='Toys')
+    toys.is_active = False
+    toys.save()
+
+    response = api_client.get(categories_endpoint_url)
+    assert response.json() == list_categories_without_toys
+
+
+def test_priority(api_client, db_test_data):
+    response = api_client.get(categories_endpoint_url)
+    assert response.json() == list_categories_expected_response
+
+    sport = ParentCategories.objects.get(name='Sport')
+    sport.priority = 3
+    sport.save()
+
+    toys = ParentCategories.objects.get(name='Toys')
+    toys.priority = 1
+    toys.save()
+
+    response = api_client.get(categories_endpoint_url)
+    assert response.json() == list_categories_ordered_by_priority
