2c7a64c9e9f7c84bb0fdb8c0cd9b46a22b6a9a14
iMykhailychenko <igor.c.m@ukr.net>
2020-12-11 23:13:21 +0200

    delete empty files

--- frontend/assets/config.ts
+++ frontend/assets/config.ts
@@ -1,6 +1,6 @@
 const config = {
     baseUrl: {
-        dev: process.browser ? 'http://140.82.39.245/api/v1' : 'http://backend:8000/api/v1',
+        dev: process.browser ? 'http://140.82.39.245/api/v1' : 'http://140.82.39.245/api/v1',
         prod: process.browser ? 'http://140.82.39.245/api/v1' : 'http://backend:8000/api/v1',
     },
 };
