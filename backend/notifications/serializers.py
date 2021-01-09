19b8016f4e221566e1ff3d4f36f85e84669e0d50
egorfray <fatamorganaa933@gmail.com>
2021-01-09 09:42:27 +0200

    Notifications issue

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -4,7 +4,7 @@ from .models import Notifications
 
 class NotificationsSerializer(serializers.ModelSerializer):
     recipient = serializers.CharField(source="recipient.email")
-    # pub_date = serializers.CharField(source="pub_date_display")
+    pub_date = serializers.CharField(source="pub_date_display")
 
     class Meta:
         model = Notifications
@@ -12,5 +12,4 @@ class NotificationsSerializer(serializers.ModelSerializer):
             'recipient',
             'body',
             'pub_date',
-            'pub_date_display',
         ]
--- backend/notifications/tests/mocks.py
+++
@@ -1,7 +0,0 @@
-# notifications_endpoint_url_with_user = '/api/v1/notifications/'
-# notifications_expected_response_with_user = [
-#     {
-#         "recipient": "maric0naric@gmail.com",
-#         "body": "some text"
-#     }
-# ]
