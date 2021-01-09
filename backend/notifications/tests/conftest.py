de6bbb6b399f38b021930c19d9a3d2a29f72b82e
egorfray <fatamorganaa933@gmail.com>
2021-01-09 09:42:27 +0200

    Notifications issue

--- backend/notifications/tests/conftest.py
+++ backend/notifications/tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from rest_framework.test import APIClient
 from notifications.models import Notifications
 from users.models import User
+from freezegun import freeze_time
 
 
 @pytest.fixture
@@ -26,11 +27,22 @@ def test_user_2(db_test_data):
     return client
 
 
+@pytest.fixture()
+def test_user_3(db_test_data):
+    user = User.objects.get(email='jstop@gmail.com')
+    client = APIClient()
+    client.force_authenticate(user=user)
+    return client
+
+
 @pytest.fixture
+@freeze_time("2020-10-14 12:00:01")
 def db_test_data(db):
     user_1 = User.objects.create_user(first_name='Marian', last_name='Zozulia', email='maric0naric@gmail.com',
                                       password='bla bla bla 123')
     user_2 = User.objects.create_user(first_name='Egor', last_name='Fray', email='qwerty12345@gmail.com',
                                       password='123erw')
+    user_3 = User.objects.create_user(first_name='Igor', last_name='Michylechenko', email='jstop@gmail.com',
+                                      password='jstop')
     Notifications.objects.create(recipient=user_1, body='some text', viewed=True)
     Notifications.objects.create(recipient=user_2, body='some cool text', viewed=False)
--- backend/notifications/tests/tests.py
+++ backend/notifications/tests/tests.py
@@ -1,16 +1,26 @@
 from rest_framework import status
-from freezegun import freeze_time
 
 
-@freeze_time("2020-12-04")
 def test_list_notifications_for_user_1(api_client, test_user_1, db_test_data):
     response = test_user_1.get('/api/v1/notifications/')
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == [{"recipient": "maric0naric@gmail.com", "body": "some text"}]
+    assert response.json() == [{"recipient": "maric0naric@gmail.com", "pub_date": "2020-10-14 12:00:01",
+                                "body": "some text"}]
 
 
-@freeze_time("2020-12-04")
 def test_list_notifications_for_user_2(api_client, test_user_2, db_test_data):
     response = test_user_2.get('/api/v1/notifications/')
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == [{"recipient": "qwerty12345@gmail.com", "body": "some cool text"}]
+    assert response.json() == [{"recipient": "qwerty12345@gmail.com", "pub_date": "2020-10-14 12:00:01",
+                                "body": "some cool text"}]
+
+
+def test_empty_list(api_client, test_user_3, db_test_data):
+    response = test_user_3.get('/api/v1/notifications/')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
+
+
+def test_unauthorized(api_client, db_test_data):
+    response = api_client.get('/api/v1/notifications/')
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
