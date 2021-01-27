df4e848cbfa369791a3e5ce286b5ea25b61fbac1
Ihor Mykhailychenko <imykhailychenko@int.codinsula.com>
2021-01-27 12:14:07 +0200

    axios delete issue

--- frontend/assets/api.ts
+++ frontend/assets/api.ts
@@ -30,7 +30,7 @@ const api = {
         list: (id: string): Promise<AxiosResponse<IComment[]>> => axios.get(`/comments/${id}`),
         create: (id: string, body: { body: string; offer_id: string; images: { url: string }[] }): Promise<AxiosResponse<void>> =>
             axios.post(`/comments/${id}/`, body),
-        delete: (id: number): Promise<AxiosResponse<void>> => axios.delete(`/comments/${id}`, {}),
+        delete: (id: number): Promise<AxiosResponse<void>> => axios.delete(`/comments/${id}`, { data: null }),
         reply: (id: number, body: { body: string; images: { url: string }[] }): Promise<AxiosResponse<void>> =>
             axios.post(`/comments/${id}/reply/`, body),
         like: (id: number): Promise<AxiosResponse<void>> => axios.patch(`/comments/${id}/like/`),
