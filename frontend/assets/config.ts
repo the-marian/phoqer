32238e6e0c0ced34dd9c2cffab3225bbcccd47b7
Ihor Mykhailychenko <imykhailychenko@int.codinsula.com>
2021-01-29 12:45:13 +0200

    fastapi ssr

--- frontend/assets/config.ts
+++ frontend/assets/config.ts
@@ -2,7 +2,11 @@ const config = {
     baseUrl: {
         development: (version = 'v1'): string => 'http://phoqer.com/api/' + version,
         production: (version = 'v1'): string =>
-            (process.browser ? 'http://phoqer.com/api/' : 'http://backend:8000/api/') + version,
+            (process.browser
+                ? 'http://phoqer.com/api/'
+                : version === 'v1'
+                ? 'http://backend:8000/api/'
+                : 'http://fastapi:8001/api/') + version,
     },
     uploadsUrl: (version = 'v1'): string => `http://140.82.39.245/api/${version}/upload/`,
     host: 'http://phoqer.com/',
