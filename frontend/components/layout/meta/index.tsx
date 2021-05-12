686d95e3a52091702f02bc8a1356469e14bf0d39
iMykhailychenko <igor.c.m@ukr.net>
2021-05-12 19:02:11 +0300

    404 page

--- frontend/components/layout/meta/index.tsx
+++ frontend/components/layout/meta/index.tsx
@@ -62,16 +62,16 @@ const Meta = ({ title, description, keywords, icon, h1 = '' }: IProps): ReactEle
 
                 {/*Open Graph / Facebook*/}
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content={url()} />
+                <meta property="og:url" content={url(history.locale)} />
                 <meta property="og:title" content={title?.trim() ? `${title} | Phoqer` : 'Phoqer'} />
                 <meta property="og:description" content={description || trans('site_desc')} />
                 <meta property="og:image" content={icon || `/about.jpg`} />
-                <meta property="og:url" content={url()} />
+                <meta property="og:url" content={url(history.locale)} />
                 <meta property="og:site_name" content="Phoqer" />
 
                 {/*Twitter*/}
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content={url()} />
+                <meta property="twitter:url" content={url(history.locale)} />
                 <meta property="twitter:title" content={title?.trim() ? `${title} | Phoqer` : 'Phoqer'} />
                 <meta property="twitter:description" content={description || trans('site_desc')} />
                 <meta property="twitter:image" content={icon || `/about.jpg`} />
@@ -106,7 +106,7 @@ const Meta = ({ title, description, keywords, icon, h1 = '' }: IProps): ReactEle
 
                 <link rel="mask-icon" href="/phoqer-512.png" color="#007aff" />
 
-                <link rel="canonical" href={url()} />
+                <link rel="canonical" href={url(history.locale)} />
                 <link rel="alternate" hrefLang="x-default" href={url()} />
                 <link rel="alternate" hrefLang="en" href={url('en')} />
                 <link rel="alternate" hrefLang="ru" href={url('ru')} />
@@ -117,10 +117,10 @@ const Meta = ({ title, description, keywords, icon, h1 = '' }: IProps): ReactEle
                 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <meta name="apple-mobile-web-app-title" content={title?.trim() ? `${title} | Phoqer` : 'Phoqer'} />
                 <meta name="description" content={description || trans('site_desc')} />
-                <meta name="format-detection" content="telephone=no" />
+                <meta name="format-detection" content="telephone=yes" />
                 <meta name="mobile-web-app-capable" content="yes" />
                 <meta name="msapplication-TileColor" content="#007aff" />
-                <meta name="msapplication-tap-highlight" content="no" />
+                <meta name="msapplication-tap-highlight" content="yes" />
             </Head>
             <h1 className={css.title}>{h1 ? h1 + '. ' + trans('site_desc') : trans('site_desc')}</h1>
         </>
