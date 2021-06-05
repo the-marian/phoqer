609eb969bbdd91249efe4285129eabf685abb5ae
iMykhailychenko <igor.c.m@ukr.net>
2021-06-06 02:10:28 +0300

    add git hooks

--- frontend/.husky/pre-commit.sh
+++ frontend/.husky/pre-commit.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+
 . "$(dirname "$0")/_/husky.sh"
 
-cd frontend || exit
+cd ./frontend/ || exit
 npm run lint && npm run test
---
+++ frontend/components/common/auth/login-form/login-form.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '../../../../tests/tests.utils';
+import LoginForm from './index';
+
+describe('LoginForm component', () => {
+    it('matches snapshot', () => {
+        const { asFragment } = render(<LoginForm />, {});
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
