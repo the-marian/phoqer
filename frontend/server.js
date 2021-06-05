02eaaa05a6b5067cbe883e85108864b6bf745b71
iMykhailychenko <igor.c.m@ukr.net>
2021-06-05 19:53:19 +0300

    custom server

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
+const PORT = 4000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -10,14 +11,15 @@ app.prepare().then(() => {
     createServer((req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
-
         process.on('SIGILL', signal => {
+            console.log('---LOGGER-FOR-MARIAN---');
             console.log(signal);
         });
-    }).listen(4000, err => {
+    }).listen(PORT, err => {
         if (err) {
             console.log(err);
             throw err;
         }
+        console.log('Start front-end dich on port: ' + PORT);
     });
 });
