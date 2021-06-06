18be22c0da5750ca177f844ed405ac8f7d350d62
Ihor Mykhailychenko <igor.c.m@ukr.net>
2021-06-06 03:32:37 +0300

    Meta tegs (#182)

---
+++ frontend/next-sitemap.js
@@ -0,0 +1,18 @@
+module.exports = {
+    siteUrl: 'http://phoqer.com',
+    generateRobotsTxt: true,
+    alternateRefs: [
+        {
+            href: 'http://phoqer.com/en',
+            hreflang: 'en',
+        },
+        {
+            href: 'http://phoqer.com/ru',
+            hreflang: 'ru',
+        },
+        {
+            href: 'http://phoqer.com/ua',
+            hreflang: 'ua',
+        },
+    ],
+};
