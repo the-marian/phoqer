30e92cd9ddac79b8a5e301ae81c3243316516110
iMykhailychenko <igor.c.m@ukr.net>
2021-06-10 00:17:20 +0300

    add new fields to offer form

--- frontend/components/common/construction/index.tsx
+++ frontend/components/common/construction/index.tsx
@@ -40,7 +40,7 @@ interface IProps {
 const Construction = ({ text }: IProps): ReactElement => {
     const css = useStyles();
     const trans = useTrans();
-    const [img, setImg] = useState('');
+    const [img, setImg] = useState('/spinner.gif');
 
     useEffect(() => {
         if (process.browser) {
