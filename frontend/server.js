e1276630c81b80999d00443550240bb6391e6aae
imykhailychenko <igor.c.m@ukr.net>
2021-08-02 19:00:44 +0300

    Edit Dockerfile for frontend. Maybe this can increase the speed of front-end deployment

--- frontend/server.js
+++ frontend/server.js
@@ -3,6 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 
 const PORT = 4000;
+const HOST = process.env.HOST || '0.0.0.0';
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,19 +12,16 @@ app.prepare().then(() => {
     createServer((req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl).catch(error => {
-            console.log('---LOGGER-FOR-MARIAN---');
-            console.log(error);
+            console.log('---LOGGER-FOR-MARIAN---', error);
         });
         process.on('SIGILL', signal => {
-            console.log('---LOGGER-FOR-MARIAN---');
-            console.log(signal);
+            console.log('---LOGGER-FOR-MARIAN---', signal);
         });
-    }).listen(PORT, err => {
+    }).listen(PORT, HOST, err => {
         if (err) {
-            console.log('---LOGGER-FOR-MARIAN---');
-            console.log(err);
+            console.log('---LOGGER-FOR-MARIAN---', err);
             throw err;
         }
-        console.log('Front-end is run on port: ' + PORT);
+        console.log('Front-end is run on port: ' + PORT + ', end host: ' + HOST);
     });
 });
