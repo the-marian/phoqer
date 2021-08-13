c087151ae68117eaff8e3fcb67eb776a5c85114b
imykhailychenko <igor.c.m@ukr.net>
2021-08-13 15:32:42 +0300

    change logs

--- frontend/server.js
+++ frontend/server.js
@@ -4,24 +4,29 @@ const next = require('next');
 
 const PORT = 4000;
 const HOST = process.env.HOST || '0.0.0.0';
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-    createServer((req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl).catch(error => {
-            console.log('---LOGGER-FOR-MARIAN---', error);
+app.prepare()
+    .then(() => {
+        createServer((req, res) => {
+            const parsedUrl = parse(req.url, true);
+            handle(req, res, parsedUrl).catch(error => {
+                console.log('---LOGGER-FOR-MARIAN---', error);
+            });
+        }).listen(PORT, err => {
+            if (err) {
+                console.log('---LOGGER-FOR-MARIAN---', err);
+                throw err;
+            }
+
+            process.on('SIGILL', signal => {
+                console.log('---LOGGER-FOR-MARIAN---', signal);
+            });
+            console.log('Front-end is run on port: ' + PORT + ', end host: ' + HOST);
         });
-        process.on('SIGILL', signal => {
-            console.log('---LOGGER-FOR-MARIAN---', signal);
-        });
-    }).listen(PORT, err => {
-        if (err) {
-            console.log('---LOGGER-FOR-MARIAN---', err);
-            throw err;
-        }
-        console.log('Front-end is run on port: ' + PORT + ', end host: ' + HOST);
+    })
+    .catch(error => {
+        console.log('---LOGGER-FOR-MARIAN---', error);
     });
-});
