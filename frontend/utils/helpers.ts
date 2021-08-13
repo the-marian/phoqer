8e2c524a407f1a62cfc9617575f02b43bdfaf8ff
imykhailychenko <igor.c.m@ukr.net>
2021-08-13 15:26:55 +0300

    change logs

--- frontend/utils/helpers.ts
+++ frontend/utils/helpers.ts
@@ -152,13 +152,15 @@ export const parseCookie = <T>(value = '', key = 'phoqer_auth', parsed = false):
 // ----------------------------------------------
 // custom console log for site identity
 export const logger = (): void => {
-    if (process.env.NODE_ENV === 'production' && process.browser) console.clear();
-    console.log(
-        '%c Phoqer %c v0.0.1 Made with love ...',
-        'padding: 6px 15px; border-radius: 10px; background: #eee; text-transform: uppercase; color: #007aff; font-size: 1rem; font-weight: 600; font-family: Montserrat, sans-serif',
-        'color: #DB162F; font-size: 0.8rem;',
-    );
-    console.log('%c Coming soon... The website is under construction', 'font-size: 1.2rem;');
+    if (process.browser) {
+        if (process.env.NODE_ENV === 'production') console.clear();
+        console.log(
+            '%c Phoqer %c v0.0.1 Made with love ...',
+            'padding: 6px 15px; border-radius: 10px; background: #eee; text-transform: uppercase; color: #007aff; font-size: 1rem; font-weight: 600; font-family: Montserrat, sans-serif',
+            'color: #DB162F; font-size: 0.8rem;',
+        );
+        console.log('%c Coming soon... The website is under construction', 'font-size: 1.2rem;');
+    }
 };
 
 // ----------------------------------------------
