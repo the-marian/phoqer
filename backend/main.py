9dd6680461cec4fbf49071d0a25352d0ce41c9ff
Marian <maryan.zozulia@gmail.com>
2021-10-24 15:27:47 +0200

    test

--- backend/main.py
+++ backend/main.py
@@ -45,6 +45,6 @@ async def startup() -> None:
 async def shutdown() -> None:
     await database.disconnect()
 
-
+# asdfsadf
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
