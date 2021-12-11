0988e160630e8e1dcfcef607b4ebebc58edfd86f
Marian <maryan.zozulia@gmail.com>
2021-12-11 15:33:41 +0100

    small fix

--- backend/popular_searches/popular.py
+++ backend/popular_searches/popular.py
@@ -2,6 +2,7 @@ import random
 from typing import Dict, List
 
 from fastapi import APIRouter
+
 from config import POPULAR_SEARCH_SIZE
 
 router = APIRouter(
@@ -12,18 +13,72 @@ router = APIRouter(
 
 @router.get("")
 async def get_popular_searches() -> Dict[str, List[str]]:
-    ua = ["Iphone", "PS5", "VR", "Каршерінг", "Квартира",
-          "Гітара", "Офіс", "AR", "Ігри", "Пентхаус",
-          "Зброя", "Сноуборд", "Телевізор", "Робот-пилосос", "Samsung",
-          "DJI Mavic", "Galaxy Z Flip", "Навушники", "Canon 500px", "3d принтер"]
-    en = ["Iphone", "PS5", "VR", "Carrent", "Apartment",
-          "Guitar", "AR", "Laptop", "Game", "Penthouse",
-          "Guns", "Snowboard", "TV", "Vacuum cleaner", "Samsung",
-          "DJI Mavic", "Galaxy Z Flip", "Headphones", "Canon 500px", "3d"]
-    ru = ["Iphone", "PS5", "VR", "Каршеринг", "Квартира",
-          "Гитара", "Офис", "AR", "Игры", "Пентхаус",
-          "Оружие", "Сноуборд", "Телевизор", "Робот-пылесос", "Samsung",
-          "DJI Mavic", "Galaxy Z Flip", "Наушники", "Canon 500px", "3d принтер"]
+    ua = [
+        "Iphone",
+        "PS5",
+        "VR",
+        "Машина",
+        "AirPods Pro",
+        "Гітара",
+        "Apple Watch",
+        "Nintendo Switch",
+        "Настільні ігри",
+        "Кавомашина",
+        "Велосипед",
+        "Сноуборд",
+        "Телевізор",
+        "Робот-пилосос",
+        "Samsung",
+        "DJI Mavic",
+        "Galaxy Z Flip",
+        "Навушники",
+        "Canon",
+        "3d принтер",
+    ]
+    en = [
+        "Iphone",
+        "PS5",
+        "VR",
+        "Carrent",
+        "AirPods Pro",
+        "Guitar",
+        "Nintendo Switch",
+        "Laptop",
+        "Board game ",
+        "Coffee maker",
+        "Bike",
+        "Snowboard",
+        "TV",
+        "Vacuum cleaner",
+        "Samsung",
+        "DJI Mavic",
+        "Galaxy Z Flip",
+        "Headphones",
+        "Canon",
+        "3d printer",
+    ]
+    ru = [
+        "Iphone",
+        "PS5",
+        "VR",
+        "Машина",
+        "Apple Watch",
+        "Гитара",
+        "AirPods Pro",
+        "Nintendo Switch",
+        "Настольные игры",
+        "Пентхаус",
+        "Велосипед",
+        "Сноуборд",
+        "Телевизор",
+        "Робот-пылесос",
+        "Samsung",
+        "DJI Mavic",
+        "Galaxy Z Flip",
+        "Наушники",
+        "Canon 500px",
+        "3d принтер",
+    ]
     return {
         "ua": random.sample(ua, POPULAR_SEARCH_SIZE),
         "en": random.sample(en, POPULAR_SEARCH_SIZE),
