6c294e9cc1b900fa06a76098036fde6f250aebaa
egorfray <fatamorganaa933@gmail.com>
2021-12-11 16:14:41 +0200

    Add 20 popular searches

--- backend/popular_searches/popular.py
+++ backend/popular_searches/popular.py
@@ -2,6 +2,7 @@ import random
 from typing import Dict, List
 
 from fastapi import APIRouter
+from config import POPULAR_SEARCH_SIZE
 
 router = APIRouter(
     prefix="/popular-searches",
@@ -11,11 +12,20 @@ router = APIRouter(
 
 @router.get("")
 async def get_popular_searches() -> Dict[str, List[str]]:
-    ua = ["Iphone", "PS5", "VR", "Каршерінг", "Квартира", "Гітара", "Офіс", "AR", "Ігри"]
-    en = ["Iphone", "PS5", "VR", "Carrent", "Apartment", "Guitar", "AR", "Laptop", "Game"]
-    ru = ["Iphone", "PS5", "VR", "Каршеринг", "Квартира", "Гитара", "Офис", "AR", "Игры"]
+    ua = ["Iphone", "PS5", "VR", "Каршерінг", "Квартира",
+          "Гітара", "Офіс", "AR", "Ігри", "Пентхаус",
+          "Зброя", "Сноуборд", "Телевізор", "Робот-пилосос", "Samsung",
+          "DJI Mavic", "Galaxy Z Flip", "Навушники", "Canon 500px", "3d принтер"]
+    en = ["Iphone", "PS5", "VR", "Carrent", "Apartment",
+          "Guitar", "AR", "Laptop", "Game", "Penthouse",
+          "Guns", "Snowboard", "TV", "Vacuum cleaner", "Samsung",
+          "DJI Mavic", "Galaxy Z Flip", "Headphones", "Canon 500px", "3d"]
+    ru = ["Iphone", "PS5", "VR", "Каршеринг", "Квартира",
+          "Гитара", "Офис", "AR", "Игры", "Пентхаус",
+          "Оружие", "Сноуборд", "Телевизор", "Робот-пылесос", "Samsung",
+          "DJI Mavic", "Galaxy Z Flip", "Наушники", "Canon 500px", "3d принтер"]
     return {
-        "ua": random.sample(ua, len(ua)),
-        "en": random.sample(en, len(en)),
-        "ru": random.sample(ru, len(ru)),
+        "ua": random.sample(ua, POPULAR_SEARCH_SIZE),
+        "en": random.sample(en, POPULAR_SEARCH_SIZE),
+        "ru": random.sample(ru, POPULAR_SEARCH_SIZE),
     }
