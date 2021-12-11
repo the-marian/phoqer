50c725ebe3e80352a3cd7d2bdf500ccbf11d77a2
EgorFray <57326400+EgorFray@users.noreply.github.com>
2021-12-11 16:38:32 +0200

    Add 20 popular searches (#238)
    * Add 20 popular searches
    
    * small fix
    
    Co-authored-by: Marian <maryan.zozulia@gmail.com>

--- backend/popular_searches/popular.py
+++ backend/popular_searches/popular.py
@@ -3,6 +3,8 @@ from typing import Dict, List
 
 from fastapi import APIRouter
 
+from config import POPULAR_SEARCH_SIZE
+
 router = APIRouter(
     prefix="/popular-searches",
     tags=["popular-searches"],
@@ -11,11 +13,74 @@ router = APIRouter(
 
 @router.get("")
 async def get_popular_searches() -> Dict[str, List[str]]:
-    ua = ["Iphone", "PS5", "VR", "Каршерінг", "Квартира", "Гітара", "Офіс", "AR", "Ігри"]
-    en = ["Iphone", "PS5", "VR", "Carrent", "Apartment", "Guitar", "AR", "Laptop", "Game"]
-    ru = ["Iphone", "PS5", "VR", "Каршеринг", "Квартира", "Гитара", "Офис", "AR", "Игры"]
+    ua = [
+        "Iphone",
+        "PS5",
+        "VR",
+        "Машина",
+        "AirPods Pro",
+        "Гітара",
+        "Apple Watch",
+        "Nintendo Switch",
+        "Настільні ігри",
+        "Кавомашина",
+        "Велосипед",
+        "Сноуборд",
+        "Телевізор",
+        "Робот-пилосос",
+        "Samsung",
+        "DJI Mavic",
+        "Galaxy Z Flip",
+        "Навушники",
+        "Canon",
+        "3d принтер",
+    ]
+    en = [
+        "Iphone",
+        "PS5",
+        "VR",
+        "Carrent",
+        "AirPods Pro",
+        "Guitar",
+        "Nintendo Switch",
+        "Laptop",
+        "Board game ",
+        "Coffee maker",
+        "Bike",
+        "Snowboard",
+        "TV",
+        "Vacuum cleaner",
+        "Samsung",
+        "DJI Mavic",
+        "Galaxy Z Flip",
+        "Headphones",
+        "Canon",
+        "3d printer",
+    ]
+    ru = [
+        "Iphone",
+        "PS5",
+        "VR",
+        "Машина",
+        "Apple Watch",
+        "Гитара",
+        "AirPods Pro",
+        "Nintendo Switch",
+        "Настольные игры",
+        "Пентхаус",
+        "Велосипед",
+        "Сноуборд",
+        "Телевизор",
+        "Робот-пылесос",
+        "Samsung",
+        "DJI Mavic",
+        "Galaxy Z Flip",
+        "Наушники",
+        "Canon 500px",
+        "3d принтер",
+    ]
     return {
-        "ua": random.sample(ua, len(ua)),
-        "en": random.sample(en, len(en)),
-        "ru": random.sample(ru, len(ru)),
+        "ua": random.sample(ua, POPULAR_SEARCH_SIZE),
+        "en": random.sample(en, POPULAR_SEARCH_SIZE),
+        "ru": random.sample(ru, POPULAR_SEARCH_SIZE),
     }
