1109d8d807c20420dda525d763ded9a0d38b0352
imykhailychenko <igor.c.m@ukr.net>
2021-12-14 12:51:10 +0200

    next pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,6 @@
 const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
 
-const pwa =
-    process.env.NODE_ENV === 'development'
-        ? {}
-        : withPWA({
-              pwa: {
-                  dest: '/public',
-                  runtimeCaching,
-              },
-          });
-
-module.exports = {
+module.exports = withPWA({
     async redirects() {
         return [
             {
@@ -43,5 +32,4 @@ module.exports = {
     images: {
         domains: ['phoqer.com'],
     },
-    ...pwa,
-};
+});
--- frontend/server.js
+++ frontend/server.js
@@ -1,29 +1,24 @@
 const next = require('next');
 
 const { createServer } = require('http');
+const { join } = require('path');
 const { parse } = require('url');
 
 const PORT = 4000;
-const HOST = process.env.HOST || '0.0.0.0';
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
-app.prepare()
-    .then(() => {
-        createServer((req, res) => {
-            const parsedUrl = parse(req.url, true);
-            handle(req, res, parsedUrl).catch(error => {
-                console.log('Frontend server error', error);
-            });
-        }).listen(PORT, err => {
-            if (err) {
-                console.log('Frontend server error', err);
-                throw err;
-            }
-            console.log('Front-end is run on port: ' + PORT + ', end host: ' + HOST);
-        });
-    })
-    .catch(error => {
-        console.log('Frontend server error', error);
-    });
+app.prepare().then(() => {
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname } = parsedUrl;
+
+        if (pathname === '/sw.js' || /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)) {
+            const filePath = join(__dirname, '.next', pathname);
+            app.serveStatic(req, res, filePath);
+        } else {
+            handle(req, res, parsedUrl);
+        }
+    }).listen(PORT, () => console.log('Front-end is run on port: ' + PORT));
+});
